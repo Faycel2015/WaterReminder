e3008071072a5c721dc69b17f4c8893ca5b1d863
Faycel2015 <faycel.ayech@gmail.com>
2025-02-15 00:07:40 +0100

    Initial commit of Water Reminder App

---
+++ WaterReminder/WaterReminder/WaterReminderApp.swift
@@ -0,0 +1,17 @@
+//
+//  WaterReminderApp.swift
+//  WaterReminder
+//
+//  Created by FayTek on 2/15/25.
+//
+
+import SwiftUI
+
+@main
+struct WaterReminderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
